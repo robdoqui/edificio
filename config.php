03e28a8ed91bce17ac420318d7ac2510a1eaf2df
robdoqui <rob_jimenez@hotmail.com>
2024-05-14 14:25:00 -0400

    Update config.php

--- config.php
+++ config.php
@@ -11,7 +11,7 @@
 
 // Creando una nueva conexión a la base de datos.
 /**$conn = new mysqli("127.0.0.1", "root", "password", "residentes"); */
-$conn = new mysqli("192.168.1.33", "root", "qweartemisa", "edificio");
+$conn = new mysqli("localhost", "root", "qweartemisa", "edificio");
 // Comprobando si hay un error de conexión.
 if ($conn->connect_error) {
     die( 'Error de conexion ' . $conn->connect_error);
