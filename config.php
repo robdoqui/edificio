5358600caadb9bcef90c9ba5ac85d76eb9803914
robdoqui <rob_jimenez@hotmail.com>
2024-05-14 13:37:13 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Conexión a base de datos de MySQL con PHP
+ *
+ * @author mroblesdev
+ * @link https://github.com/mroblesdev/server-side-php
+ * @license: MIT
+ */
+
+
+// Creando una nueva conexión a la base de datos.
+/**$conn = new mysqli("127.0.0.1", "root", "password", "residentes"); */
+$conn = new mysqli("127.0.0.1", "root", "qweartemisa", "edificio");
+// Comprobando si hay un error de conexión.
+if ($conn->connect_error) {
+    die( 'Error de conexion ' . $conn->connect_error);
+    exit;
+}
